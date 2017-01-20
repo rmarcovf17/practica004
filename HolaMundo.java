80fb1481fdb296820347afba5e50cb6692e713d3
RODRIGO MARCO <r.marco5@usp.ceu.es>
2017-01-20 19:42:13 +0100

    nueva forma de imprimir HolaMundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,3 @@
  public class HolaMundo{
-      public static void main(String[] args){ System.out.println("Hola Mundo");}
+      public static void main(String[] args){ System.out.print("Nuevo Hola Mundo");}
 }
